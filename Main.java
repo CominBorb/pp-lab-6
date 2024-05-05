142a210b68fbef81409a4dba7cbd0d1fd390698f
CominBorb <norbert.oledzki@gmail.com>
2024-05-05 12:06:34 +0200

    ok

--- Main.java
+++ Main.java
@@ -1,33 +1,30 @@
 import company.models.Manager;
 import company.models.Worker;
+import company.abstracts.Employee;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
 
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-        Worker worker1 = new Worker("Maciek", 3500, 1);
-        Worker worker2 = new Worker("Jasiek", 3600, 2);
-        Worker worker3 = new Worker("Tadek", 4500, 3);
-        Worker worker4 = new Worker("Kamil", 6000, 4);
+        Worker worker1 = new Worker("Maciek", 3500, 1, "2023-01-01", "Worker");
+        Worker worker2 = new Worker("Jasiek", 3600, 2, "2023-02-01", "Worker");
+        Worker worker3 = new Worker("Tadek", 4500, 3, "2023-03-01", "Worker");
+        Worker worker4 = new Worker("Kamil", 6000, 4, "2023-04-01", "Worker");
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
 
+        Manager manager = new Manager("Janusz", 7000, 5, "2023-05-01", "Manager");
+        employees.add(manager);
 
-        Manager manager = new Manager("Janusz", 7000, 5);
-
-
-        System.out.println("Wynagrodzenie 1 pracownika " + worker1.getSalary());
-        worker1.work();
-
-        System.out.println("Wynagrodzenie 2 pracownika " + worker2.getSalary());
-        worker2.work();
-
-        System.out.println("Wynagrodzenie 3 pracownika " + worker3.getSalary());
-        worker3.work();
-
-        System.out.println("Wynagrodzenie 5 pracownika " + worker4.getSalary());
-        worker4.work();
-
-        System.out.println("Wynagrodzenie menadzera " + manager.getSalary());
-        manager.work();
+        for (Employee employee : employees) {
+            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " +
+                    employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " +
+                    employee.getSalary() + ")");
+            employee.work();
+        }
     }
 }
--- company/models/Manager.java
+++ company/models/Manager.java
@@ -1,16 +1,14 @@
 package company.models;
+
 import company.abstracts.Employee;
 
-public class Manager extends Employee 
-{
-    public Manager(String name, double salary, int id) 
-    {
-        super(name, salary, id);
+public class Manager extends Employee {
+    public Manager(String name, double salary, int id, String hireDate, String position) {
+        super(name, salary, id, hireDate, position);
     }
 
     @Override
-    public void work() 
-    {
+    public void work() {
         System.out.println("Manager " + getName() + " is managing...");
     }
 }
